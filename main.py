be8b74c673f3d7a12b4fe6c69ff960833e7f8c85
Vital Shved <Skandinav_by@mail.ru>
2021-03-04 20:57:33 +0300

    column search

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 # Control of performance discipline
 
-
 from docx import Document
 
 
 file = open('events.docx', 'rb')
-document = Document(file)
-print(document)
+doc = Document(file)
 file.close()
+
+'''Поиск по исполнителю'''
+for i in range(1, len(doc.tables)):
+    for j in range(1, len(doc.tables[i].rows)):
+        print(doc.tables[i].rows[j].cells[3].text)
+
+'''Поиск по контролю'''
+for i in range(1, len(doc.tables)):
+    for j in range(1, len(doc.tables[i].rows)):
+        print(doc.tables[i].rows[j].cells[4].text)
+
+'''Поиск по сроку исполнения'''
+for i in range(1, len(doc.tables)):
+    for j in range(1, len(doc.tables[i].rows)):
+        print(doc.tables[i].rows[j].cells[2].text)
+
 
